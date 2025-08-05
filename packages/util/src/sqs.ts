c7b82f003fc6b5e986daba3337d5fa37e75119ce
doominkim <kimduumin@gmail.com>
2025-08-05 20:24:50 +0900

    feat: SQS 채팅 메시지 데이터 구조 개선 및 중복 ID 생성 로직 수정
    - ChatMessageData 인터페이스의 필드를 선택적으로 변경하여 유연성 향상
    - 중복 ID 생성 로직을 개선하여 cid 및 type을 활용하도록 수정
    - 불필요한 로그 메시지 제거 및 오류 처리 개선

--- packages/util/src/sqs.ts
+++ packages/util/src/sqs.ts
@@ -4,32 +4,20 @@ import {
   ReceiveMessageCommand,
   DeleteMessageCommand,
 } from "@aws-sdk/client-sqs";
+import { randomUUID } from "crypto";
 
 export interface ChatMessageData {
   type: string;
-  cid: string;
+  cid?: string;
   ctime: number;
-  msg: string;
-  uid: string;
-  profile: {
-    userIdHash: string;
-    nickname: string;
-    profileImageUrl: string;
-    userRoleCode: string;
-    badge: any;
-    title: any;
-    verifiedMark: boolean;
-    activityBadges: any[];
-    streamingProperty: any;
-    viewerBadges: any[];
-  };
+  msg?: string;
+  uid?: string;
+  profile?: any;
   extras: {
-    osType: string;
-    chatType: string;
-    streamingChannelId: string;
-    emojis: any;
-    extraToken: string;
+    streamingChannelId?: string;
+    [key: string]: any;
   };
+  [key: string]: any;
 }
 
 export class SqsChatClient {
@@ -43,15 +31,32 @@ export class SqsChatClient {
 
   async sendChat(chat: ChatMessageData) {
     const groupId = chat.extras?.streamingChannelId;
-    const dedupId =
-      chat.ctime && chat.uid ? `${chat.ctime}_${chat.uid}` : undefined;
-    if (!groupId || !dedupId) {
-      console.error(
-        "[SqsChatClient] MessageGroupId 또는 MessageDeduplicationId가 없습니다.",
-        { groupId, dedupId, chat }
-      );
+    let dedupId: string | undefined;
+    if (chat.ctime && chat.uid) {
+      dedupId = `${chat.ctime}_${chat.uid}`;
+    } else if (chat.ctime && chat.cid) {
+      dedupId = `${chat.ctime}_${chat.cid}`;
+    } else if (chat.ctime && chat.type) {
+      dedupId = `${chat.ctime}_${chat.type}`;
+    } else {
+      dedupId = randomUUID();
+    }
+
+    if (!groupId) {
+      console.error("[SqsChatClient] MessageGroupId가 없습니다.", {
+        groupId,
+        chat,
+      });
       return;
     }
+    if (!dedupId) {
+      console.error("[SqsChatClient] MessageDeduplicationId가 없습니다.", {
+        dedupId,
+        chat,
+      });
+      return;
+    }
+
     const params = {
       QueueUrl: this.queueUrl,
       MessageBody: JSON.stringify(chat),
